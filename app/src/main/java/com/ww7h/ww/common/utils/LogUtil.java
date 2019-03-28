ec4c91178f08b315479e2f74c4a9e3310d04647a
v_wuwei01 <v_wuwei01@baidu.com>
2019-03-28 16:29:24 +0800

    新增日志打印工具类，通过调用updateLevel控制打印等级

---
+++ app/src/main/java/com/ww7h/ww/common/utils/LogUtil.java
@@ -0,0 +1,57 @@
+package com.ww7h.ww.common.utils;
+import android.util.Log;
+
+/**
+ * Created by: Android Studio.
+ * Project Nam: Android-common
+ * PackageName: com.ww7h.ww.common.utils
+ * DateTime: 2019/3/28 16:11
+ *
+ * @author ww
+ */
+public class LogUtil {
+    public static final int VERBOSE = 1;
+    public static final int DEBUG = 2;
+    public static final int INFO = 3;
+    public static final int WARN = 4;
+    public static final int ERROR = 5;
+    public static final int NOTHING = 6;
+    public static final int EVERYTHING = 0;
+
+    private static int level = EVERYTHING;
+
+    public static void updateLevel(int l) {
+        level = l;
+    }
+
+    public static void v(String tag, String msg){
+        if(level <= VERBOSE){
+            Log.v(tag, msg);
+        }
+    }
+
+    public static void d(String tag, String msg){
+        if(level <= DEBUG){
+            Log.d(tag, msg);
+        }
+    }
+
+    public static void i(String tag, String msg){
+        if(level <= INFO){
+            Log.i(tag, msg);
+        }
+    }
+
+    public static void w(String tag, String msg){
+        if(level <= WARN){
+            Log.w(tag, msg);
+        }
+    }
+
+    public static void e(String tag, String msg){
+        if(level <= ERROR){
+            Log.e(tag, msg);
+        }
+    }
+
+}
