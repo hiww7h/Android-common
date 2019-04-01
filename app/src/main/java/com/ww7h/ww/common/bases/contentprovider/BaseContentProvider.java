bd1e0affde97b14ad4d2dd9f39688b5cf45707e6
v_wuwei01 <v_wuwei01@baidu.com>
2019-04-01 16:17:59 +0800

    添加BaseContentProvider.java

---
+++ app/src/main/java/com/ww7h/ww/common/bases/contentprovider/BaseContentProvider.java
@@ -0,0 +1,48 @@
+package com.ww7h.ww.common.bases.contentprovider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by: Android Studio.
+ * Project Nam: Android-common
+ * PackageName: com.ww7h.ww.common.bases.contentprovider
+ * DateTime: 2019/3/29 20:18
+ *
+ * @author ww
+ */
+public class BaseContentProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
