0e61db4720bec9e613cc2348bed57741b92668e9
“ww7h” <318363149@qq.com>
2019-04-09 16:45:28 +0800

    使用DiffUtil刷新RecyclerView

--- app/src/main/java/com/ww7h/ww/common/bases/view/recyclerview/adapters/LoadMoreAdapter.kt
+++ app/src/main/java/com/ww7h/ww/common/bases/view/recyclerview/adapters/LoadMoreAdapter.kt
@@ -1,16 +1,12 @@
 package com.ww7h.ww.common.bases.view.recyclerview.adapters
 
 import android.content.Context
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ww7h.ww.common.R
-import java.util.ArrayList
 
-abstract class LoadMoreAdapter<VH : RecyclerViewHolder,T> (var context: Context): RecyclerView.Adapter<VH>() {
-
-    private var dataList: ArrayList<T> = ArrayList()
+abstract class LoadMoreAdapter<VH : RecyclerViewHolder,T> (var context: Context): RecyclerViewAdapter<VH, T>() {
 
     abstract fun getViewType(position: Int):Int
 
@@ -36,35 +32,8 @@ abstract class LoadMoreAdapter<VH : RecyclerViewHolder,T> (var context: Context)
         }
     }
 
-    protected abstract fun onBindViewHolder(holder: VH, position: Int, viewType: Int)
-
-
-    fun getItem(position: Int): T {
-        return dataList[position]
-    }
-
     override fun getItemCount(): Int {
         return if (dataList.size < getPageCount()) dataList.size else dataList.size + 1
-    }
-
-    fun replaceDataList(dataList: List<T>) {
-        this.dataList.clear()
-        this.dataList.addAll(dataList)
-        notifyDataSetChanged()
-    }
-
-    fun addDataList(dataList: List<T>) {
-        this.dataList.addAll(dataList)
-        notifyDataSetChanged()
-    }
-
-    fun addData(data: T) {
-        dataList.add(data)
-        notifyDataSetChanged()
-    }
-
-    fun getDataList(): List<T> {
-        return dataList
     }
 
     protected fun getPageCount() : Int {
