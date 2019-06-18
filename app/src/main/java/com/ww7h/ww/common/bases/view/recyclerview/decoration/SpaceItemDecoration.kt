1847dd0a1a62e750332531826372a219be849e7e
ww7h <ww7hvip@163.com>
2019-06-18 18:54:26 +0800

    修改rv item间距计算问题

--- app/src/main/java/com/ww7h/ww/common/bases/view/recyclerview/decoration/SpaceItemDecoration.kt
+++ app/src/main/java/com/ww7h/ww/common/bases/view/recyclerview/decoration/SpaceItemDecoration.kt
@@ -3,6 +3,9 @@ package com.ww7h.ww.common.bases.view.recyclerview.decoration
 import android.graphics.Rect
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.support.v7.widget.StaggeredGridLayoutManager
+
+
 
 /**
  * Created by ww on 2018/7/29.
@@ -13,24 +16,6 @@ class SpaceItemDecoration (space: Int,column:Int): RecyclerView.ItemDecoration()
     private var mColumn: Int = 0
 
     /**
-     * Retrieve any offsets for the given item. Each field of `outRect` specifies
-     * the number of pixels that the item view should be inset by, similar to padding or margin.
-     * The default implementation sets the bounds of outRect to 0 and returns.
-     *
-     *
-     *
-     *
-     * If this ItemDecoration does not affect the positioning of item views, it should set
-     * all four fields of `outRect` (left, top, right, bottom) to zero
-     * before returning.
-     *
-     *
-     *
-     *
-     * If you need to access Adapter for additional data, you can call
-     * [RecyclerView.getChildAdapterPosition] to get the adapter position of the
-     * View.
-     *
      * @param outRect Rect to receive the output.
      * @param view    The child view to decorate
      * @param parent  RecyclerView this ItemDecoration is decorating
@@ -39,8 +24,12 @@ class SpaceItemDecoration (space: Int,column:Int): RecyclerView.ItemDecoration()
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         super.getItemOffsets(outRect, view, parent, state)
 
+        val params = view.layoutParams as StaggeredGridLayoutManager.LayoutParams
+        // 获取item在span中的下标
+        val spanIndex = params.spanIndex
+
         outRect.bottom = mSpace
-        if (parent.getChildAdapterPosition(view) == 0) {
+        if (spanIndex < mColumn) {
             outRect.top = mSpace
         } else {
             outRect.top = 0
@@ -50,15 +39,19 @@ class SpaceItemDecoration (space: Int,column:Int): RecyclerView.ItemDecoration()
             outRect.right = mSpace
         } else {
 
-            if (parent.getChildAdapterPosition(view) % mColumn == 0) {
-                outRect.left = mSpace
-                outRect.right = mSpace / 2
-            } else if (parent.getChildAdapterPosition(view) % mColumn == mColumn - 1) {
-                outRect.left = mSpace / 2
-                outRect.right = mSpace
-            } else {
-                outRect.left = mSpace / 2
-                outRect.right = mSpace / 2
+            when {
+                spanIndex % mColumn == 0 -> {
+                    outRect.left = mSpace
+                    outRect.right = mSpace / 2
+                }
+                spanIndex % mColumn == mColumn - 1 -> {
+                    outRect.left = mSpace / 2
+                    outRect.right = mSpace
+                }
+                else -> {
+                    outRect.left = mSpace / 2
+                    outRect.right = mSpace / 2
+                }
             }
         }
 
