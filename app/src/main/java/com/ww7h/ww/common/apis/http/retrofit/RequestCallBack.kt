bd105a7428a511acf5ae10cbd66390147a5a5d4d
ww7h <ww7hvip@163.com>
2019-11-05 15:10:07 +0800

    修改网络请求回调

--- app/src/main/java/com/ww7h/ww/common/apis/http/retrofit/RequestCallBack.kt
+++ app/src/main/java/com/ww7h/ww/common/apis/http/retrofit/RequestCallBack.kt
@@ -1,5 +1,14 @@
 package com.ww7h.ww.common.apis.http.retrofit
 
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.POST
+import retrofit2.http.FormUrlEncoded
+
+
+
 interface RequestCallBack {
 
     interface HttpPostCallBack<T> {
@@ -8,4 +17,12 @@ interface RequestCallBack {
         fun postFail(t: T)
     }
 
+
+    @FormUrlEncoded
+    @POST("user/personal_list_info")
+    fun getPersonalListInfo(@Field("cur_page") page: Int): Call<Response<String>>
+
+
 }
+
+
